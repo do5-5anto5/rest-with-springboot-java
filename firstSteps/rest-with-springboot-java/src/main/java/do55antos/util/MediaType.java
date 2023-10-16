783611c3974177ee6b76bf3a8894c42aedab1e9b
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-16 13:40:41 -0300

    swagger basic configuration; swagger custom implementation

--- firstSteps/rest-with-springboot-java/src/main/java/do55antos/util/MediaType.java
+++ firstSteps/rest-with-springboot-java/src/main/java/do55antos/util/MediaType.java
@@ -2,7 +2,7 @@ package do55antos.util;
 
 public class MediaType {
 	
-	public final static String APPLICATION_JSON = "application/x-json";
-	public final static String APPLICATION_XML = "application/x-xml";
+	public final static String APPLICATION_JSON = "application/json";
+	public final static String APPLICATION_XML = "application/xml";
 	public final static String APPLICATION_YML = "application/x-yaml";
 }
