840e18da1b31cce2d6a913e8b568f791d9a431cf
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-23 17:18:31 -0300

    testing CORS

--- firstSteps/rest-with-springboot-java/src/test/java/do55antos/configs/TestConfigs.java
+++ firstSteps/rest-with-springboot-java/src/test/java/do55antos/configs/TestConfigs.java
@@ -3,5 +3,15 @@ package do55antos.configs;
 public class TestConfigs {
 
 	public static final int SERVER_PORT = 8888;
+	
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	public static final String CONTENT_TYPE_JSON= "application/json";
+	public static final String CONTENT_TYPE_XML= "application/xml";
+	public static final String CONTENT_TYPE_YML= "application/x-yaml";
+
+	public static final String ORIGIN_DO55ANTOS= "https://github.com/do5-5anto5";
+	public static final String ORIGIN_IDK= "https://idk.com/idk";
 }
  
