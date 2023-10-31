188bd343b01e8fa2a6993b356d9de9e5c6f74ca6
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-31 14:13:03 -0300

    refreshToken

---
+++ firstSteps/rest-with-springboot-java/bin/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rio de Janeiro', 'Xuxa', 'Female', 'Meneghel'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Ghandi'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Ghandi'),
+	(7, 'Kentucky - US', 'Muhamad', 'Male', 'Ali'),
+	(8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(9, 'Capivari SP - Brasil', 'Tarsila', 'Female', 'do Amaral');
