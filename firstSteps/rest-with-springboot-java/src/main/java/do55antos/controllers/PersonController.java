6d0d0b0d6e267f66d86a51cab3878485425972e4
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-31 08:47:59 -0300

    value object

--- firstSteps/rest-with-springboot-java/src/main/java/do55antos/controllers/PersonController.java
+++ firstSteps/rest-with-springboot-java/src/main/java/do55antos/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import do55antos.model.Person;
+import do55antos.data_vo_v1.PersonVO;
 import do55antos.services.PersonService;
 
 @RestController
@@ -25,26 +25,26 @@ public class PersonController {
 	private PersonService service;
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll(){
+	public List<PersonVO> findAll(){
 		return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {		
+	public PersonVO findById(@PathVariable(value = "id") Long id) {		
 		return service.findById(id);
 	}
 	
 	@PostMapping(
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE)	
-	public Person create(@RequestBody Person person) {		
+	public PersonVO create(@RequestBody PersonVO person) {		
 		return service.create(person);
 	}
 	
 	@PutMapping(
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE)	
-	public Person update(@RequestBody Person person) {		
+	public PersonVO update(@RequestBody PersonVO person) {		
 		return service.update(person);
 	}
 	
