9dc46747be649b510e6d86ec44c8d54e3a092f2a
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-12-14 20:55:35 -0300

    Dockerizing

--- firstSteps/rest-with-springboot-java/src/main/java/do55antos/Startup.java
+++ firstSteps/rest-with-springboot-java/src/main/java/do55antos/Startup.java
@@ -1,7 +1,10 @@
 /**
  * Improving skills in API RESTful
- * last commit 13, December, 2023 _ 10:25 pm (UTC -3) 
+ * last commit 14, December, 2023 _ 09:00 pm (UTC -3) 
  * 
+ * application.yml from tests updated (Dockerizing application)
+ * 
+ * (13 - December - 2023)
  * Dockerfile; docker-compose-yml; fix HATEOAS tests; 
  * 
  *  PS.:
@@ -17,13 +20,35 @@
 
 package do55antos;
 
+//import java.util.HashMap;
+//import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+
+//		Map<String, PasswordEncoder> encoders = new HashMap<>();
+//
+//		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+//				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//
+//		encoders.put("pbkdf2", pbkdf2Encoder);
+//		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+//		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+//
+//		String result1 = passwordEncoder.encode("admin123");
+//		String result2 = passwordEncoder.encode("admin234");
+//		System.out.println("My hash result1 " + result1);
+//		System.out.println("My hash result2 " + result2);
 	}
+
 }
